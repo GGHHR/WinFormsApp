9e8604dafd01bd6f668bbb297e07a446c4b7beed
2 <1241639333@qq.com>
2023-09-25 16:17:42 +0800

    1

--- v2ray订阅更新/Form1.cs
+++ v2ray订阅更新/Form1.cs
@@ -27,7 +27,8 @@ namespace v2ray订阅更新
             {
                 // 在这里执行您的异步任务
                 await new Sub_get().start("https://nodefree.org/", ".item-title a", ".section p","a1","1");
-                await new Sub_get().start("https://clashnode.com/f/freenode", "[cp-post-title] a", ".post-content-content p:nth-child(18)","a2","2");
+                await new Sub_get().start("https://clashnode.com/f/freenode", "[cp-post-title] a", ".post-content-content p:nth-child(20)","a2","2");
+                await new Sub_get().start("https://v2cross.com/", ".entry-title a", ".entry-content h5","a3","1");
             });
 
             // 关闭应用程序
--- v2ray订阅更新/Sub_get.cs
+++ v2ray订阅更新/Sub_get.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using PuppeteerSharp;
 
@@ -20,11 +21,29 @@ namespace v2ray订阅更新
             await page.GoToAsync(content);
             
             await page.WaitForSelectorAsync(el);
+            
             string content1 = await page.EvaluateFunctionAsync<string>("(selector) => { return document.querySelector(selector).textContent; }", el);
-            Console.WriteLine("标签中的内容：" + content1);
+            
+            
+            // 定义匹配URL的正则表达式模式
+            string urlPattern = @"https?://[^\s/$.?#].[^\s]*";
+        
+            // 创建正则表达式对象
+            Regex regex = new Regex(urlPattern, RegexOptions.IgnoreCase);
+
+            // 查找匹配的链接
+            MatchCollection matches = regex.Matches(content1);
+
+            // 输出匹配的链接
+            foreach (Match match in matches)
+            {
+                Console.WriteLine("链接：" + match.Value);
+                new UpSubItem().Up(match.Value,remarks,id);
+            }
+            
             browser.CloseAsync();
             
-            new UpSubItem().Up(content1,remarks,id);
+            
         }
     }
 }
