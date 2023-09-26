33fe273c1cdd3173edcb72dd790864d2a310b52c
2 <1241639333@qq.com>
2023-09-26 11:20:06 +0800

    1

--- v2ray订阅更新/Form1.Designer.cs
+++ v2ray订阅更新/Form1.Designer.cs
@@ -30,11 +30,12 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(24, 21);
+            this.textBox1.Location = new System.Drawing.Point(12, 40);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(250, 43);
@@ -42,17 +43,28 @@
             this.textBox1.Text = "D:\\APP\\v2rayN-With-Core";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(17, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "填写好v2rayn路径后重启";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(358, 95);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "订阅更新";
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.Label label1;
 
         private System.Windows.Forms.TextBox textBox1;
 
